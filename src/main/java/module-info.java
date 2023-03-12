7ca69093fef03525d91358dd1f1f8d57ddb88696
Daniel <morafabadaniel@gmail.com>
2023-03-12 18:26:24 +0100

    Primeros Pasos
    1. Copia tu proyecto actual a una nueva carpeta llamada AlquilerVehiculos-v1.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.alquilervehiculosv1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.alquilervehiculosv1 to javafx.fxml;
-    exports com.example.alquilervehiculosv1;
-}
